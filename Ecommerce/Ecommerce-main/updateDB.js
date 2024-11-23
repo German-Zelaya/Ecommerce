ea692ca3e20feef8e560a0574aa717c0fcc4e083
Dario-Vildoso <127326010+Dario-Vildoso@users.noreply.github.com>
2024-11-23 11:09:40 -0400

    Comando de Update para la BD

--- Ecommerce/Ecommerce-main/updateDB.js
+++ Ecommerce/Ecommerce-main/updateDB.js
@@ -1,16 +1,37 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./db.sqlite", (err) => {
-  if (err) {
-    console.error("Error al conectar a la base de datos:", err.message);
-  } else {
-    console.log("Conexión exitosa a la base de datos.");
-  }
-});
 
-db.close((err) => {
+const db = new sqlite3.Database("./db.sqlite");
+const db2 = new sqlite3.Database("./db2.sqlite");
+
+db2.all("SELECT * FROM products", (err, rows) => {
   if (err) {
-    console.error("Error al cerrar la conexión:", err.message);
-  } else {
-    console.log("Conexión cerrada.");
+    console.error("Error al leer datos:", err);
+    return;
   }
+
+  rows.forEach((row) => {
+    db.run(
+      "INSERT INTO products (id, code, name, price, stock, description, updateAt, thumbnail, image1, image2, size, classification, category_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        row.id,
+        row.code,
+        row.name,
+        row.price,
+        row.stock,
+        row.description,
+        row.updateAt,
+        row.thumbnail,
+        row.image1,
+        row.image2,
+        row.size,
+        row.classification,
+        row.category_id,
+      ],
+      (err) => {
+        if (err) console.error("Error al insertar datos:", err);
+      }
+    );
+  });
+
+  console.log("Datos copiados con éxito.");
 });
